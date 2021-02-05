a8bbce42e7d083c100140136eb2db079c0b3fd0e
CheersMin <74691001+CheersMin@users.noreply.github.com>
2021-02-05 12:48:22 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.con03ImgWrap').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  autoplaySpeed: 2000,
+	  prevArrow:".arrowL",
+	  nextArrow:".arrowR"
+	});
+})
